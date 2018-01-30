0f489d996465a7492a13413f40c76e273cddd2d7
darowski <darowski@iastate.edu>
2018-01-30 12:34:21 -0600

    Clean Java project with libGDX 1.9.8 available extensions: Freetype, Tools, Controllers, Box2d, Box2dlights, Ashley

---
+++ Workspace/desktop/src/battle/galaxy/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package battle.galaxy.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import battle.galaxy.BattleForTheGalaxy;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new BattleForTheGalaxy(), config);
+	}
+}
