fbc67947dc74ef5bb8ff367382449e4c8235fc2e
nhurbain <nhurbain@iastate.edu>
2018-02-16 16:50:04 -0600

    Started implementing listening for server data.

--- Workspace/core/src/battle/galaxy/EnemyPlayer.java
+++ Workspace/core/src/battle/galaxy/EnemyPlayer.java
@@ -3,6 +3,7 @@ package battle.galaxy;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 
 /*
@@ -14,7 +15,7 @@ public class EnemyPlayer extends Actor{
 	Texture texture = new Texture(Gdx.files.internal("main-ship.png"));
 	TextureRegion textureRegion = new TextureRegion(texture);
 	
-	public EnemyPlayer(float x, float y, float degrees) {
+	public EnemyPlayer(Vector2 position, Vector2 direction, float rotation) {
 		
 	}
 	
--- Workspace/core/src/data/DataController.java
+++ Workspace/core/src/data/DataController.java
@@ -1,5 +1,15 @@
 package data;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Net.Protocol;
+import com.badlogic.gdx.net.ServerSocket;
+import com.badlogic.gdx.net.ServerSocketHints;
+import com.badlogic.gdx.net.Socket;
+
 import battle.galaxy.BattleForTheGalaxy;
 
 public class DataController {
@@ -11,18 +21,37 @@ public class DataController {
 	
 	public DataController(BattleForTheGalaxy game) {
 		this.game = game;
+		
+		 new Thread(new Runnable(){
+
+	            @Override
+	            public void run() {
+	                ServerSocketHints serverSocketHint = new ServerSocketHints();
+	                ServerSocket serverSocket = Gdx.net.newServerSocket(Protocol.TCP, 8081, serverSocketHint);
+	                // Loop forever
+	                while(true){
+	                    // Create a socket
+	                    Socket socket = serverSocket.accept(null);
+	                    
+	                    // Read data from the socket into a BufferedReader
+	                    BufferedReader buffer = new BufferedReader(new InputStreamReader(socket.getInputStream())); 
+	                }
+	            }
+	        }).start();
 	}
 
 	public void updateGameData(PlayerData playerData, ProjectileData projectileData) {
 		String player = game.getJson().toJson(playerData);
+		//TODO send to server
 		String projectile = "";
 		if(projectileData != null) {
 			projectile = game.getJson().toJson(projectileData);
+			//TODO send to server
 		}
 		
-		System.out.println("Player: " + player);
-		System.out.println("Projectile: " + projectile);
 	}
+	
+	
 	
 	
 	
