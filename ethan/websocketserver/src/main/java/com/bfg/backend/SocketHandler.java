74a28956873995e6c903a920c1e19891c9ea5e18
emball <emball@iastate.edu>
2018-02-21 16:16:55 -0600

    login and broadcast

--- ethan/websocketserver/src/main/java/com/bfg/backend/SocketHandler.java
+++ ethan/websocketserver/src/main/java/com/bfg/backend/SocketHandler.java
@@ -2,6 +2,7 @@ package com.bfg.backend;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,11 +13,14 @@ import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 import com.bfg.backend.repository.UserRepository;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 
 @Controller
 public class SocketHandler extends TextWebSocketHandler {
-	List sessions = new CopyOnWriteArrayList<>();
+	List<WebSocketSession> sessions = new CopyOnWriteArrayList<>();
 	
 	@Autowired
 	private UserRepository userRepository;
@@ -25,24 +29,30 @@ public class SocketHandler extends TextWebSocketHandler {
 	public void handleTextMessage(WebSocketSession session, TextMessage message)
 			throws InterruptedException, IOException {
 
-		System.out.println(message);
+		String response = "MILK: TEST CASE -- SHOULDN'T SEE THIS. INIT PART";
 		
-		//Map value = new Gson().fromJson(message.getPayload(), Map.class);
-		/*
-		for (WebSocketSession webSocketSession : sessions) {
-			Map value = new Gson().fromJson(message.getPayload(), Map.class);
-			webSocketSession.sendMessage(new TextMessage("Hello " + value.get("name") + " !"));
+		// Make json object
+		JsonObject jsonObj = new JsonParser().parse(message.getPayload()).getAsJsonObject();
+		
+		// Test
+		testPrints(jsonObj);
+	
+		// Login
+		if(jsonObj.get("jsonType").getAsInt() == 0) {
+			User user = new User();
+			user.setName(jsonObj.get("id").getAsString());
+			user.setPass(jsonObj.get("pass").getAsString());
+			response = login(user);
+			session.sendMessage(new TextMessage(response));
 		}
-		*/
-			
-		User n = new User();
-		n.setName(message.getPayload());
-//		System.out.println(n);
-		n.setPass(message.getPayload());
-		
-		userRepository.save(n);
-		
-		session.sendMessage(new TextMessage("Saved " + message.getPayload() + "!"));
+		// Broadcast locations
+		else if(jsonObj.get("jsonType").getAsInt() == 1) {
+			broadcast(session, message);
+		}
+		// Else, ERROR
+		else {
+			System.err.println("Error!");
+		}
 	}
 
 	@Override
@@ -50,4 +60,104 @@ public class SocketHandler extends TextWebSocketHandler {
 		// the messages will be broadcasted to all users.
 		sessions.add(session);
 	}
+	
+	
+	public String login(User user) {
+		loginTests(user);
+		
+		Long id = userRepository.findByLogin(user.getName(), user.getPass());
+		if(id != null) {
+			if(userRepository.exists(id)) {
+				return "Validated";
+			}
+			else {
+				return "DENIED SUCKA";
+			}
+		}
+		else {
+			return "User does not exist. Please register";
+		}
+	}
+	
+	private void loginTests(User user) {
+		System.out.println("Username Sent: " + user.getName() + ", pass sent: " + user.getPass());
+	}
+	
+	public void broadcast(WebSocketSession session, TextMessage message) throws IOException {
+		for (WebSocketSession webSocketSession : sessions) {
+			webSocketSession.sendMessage(message);
+		}
+	}
+	
+	
+	private void testPrints(JsonObject jsonObj) {
+		System.out.println("jsonOrigin: " + jsonObj.get("jsonOrigin").getAsInt());
+		System.out.println("jsonType: " + jsonObj.get("jsonType").getAsInt());
+		System.out.println("id: " + jsonObj.get("id").getAsString());
+		System.out.println("pass: " + jsonObj.get("pass").getAsString());
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+System.out.println(message);
+
+
+
+//JSONObject json = new JSONObject.put(message.getPayload());
+
+
+
+
+//Map value = new Gson().fromJson(message.getPayload(), Map.class);
+/*
+for (WebSocketSession webSocketSession : sessions) {
+	Map value = new Gson().fromJson(message.getPayload(), Map.class);
+	webSocketSession.sendMessage(new TextMessage("Hello " + value.get("name") + " !"));
+}
+
+	
+User n = new User();
+n.setName(message.getPayload());
+//System.out.println(n);
+n.setPass(message.getPayload());
+
+userRepository.save(n);
+
+session.sendMessage(new TextMessage("Saved " + message.getPayload() + "!"));
+
+
+
+		
+//		Map value = new Gson().fromJson(message.getPayload(), Map.class);
+		
+//		System.out.println(value.get("type"));
+		
+//		session.sendMessage(new TextMessage("Saved " + value.get("Id") + "!"));
+		
+//		User n = new User();
+//		n.setName(message.getPayload());
+////		System.out.println(n);
+//		n.setPass(message.getPayload());
+//		
+//		userRepository.save(n);
+//		
+//		session.sendMessage(new TextMessage("Saved " + message.getPayload() + "!"));
+
+
+*/
--- ethan/websocketserver/src/main/java/com/bfg/backend/repository/UserRepository.java
+++ ethan/websocketserver/src/main/java/com/bfg/backend/repository/UserRepository.java
@@ -1,7 +1,12 @@
 package com.bfg.backend.repository;
 
+import java.io.Serializable;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import com.bfg.backend.User;
 
@@ -9,10 +14,22 @@ import com.bfg.backend.User;
 //This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
 //CRUD refers Create, Read, Update, Delete
 
+
+
+@Repository
 public interface UserRepository extends CrudRepository<User, Long> {
 
+	@Query(value =  "SELECT user_name FROM user WHERE user_name = ?1", nativeQuery = true)
+	String findByUsername(@Param(value = "user_name") String user_name);
 	
+	@Query(value =  "SELECT user_pass FROM user WHERE user_pass = ?1", nativeQuery = true)
+	String findByPass(@Param(value = "user_pass") String user_pass);
+	
+	
+	/* ?1 = var_1, ?2 = var_2 */
+	@Query(value =  "SELECT user_id FROM user WHERE user_name = ?1 AND user_pass = ?2", nativeQuery = true)
+	Long findByLogin(@Param(value = "user_name") String user_name, @Param(value = "user_pass") String user_pass);
+	
+//	User UserExists(String name);
 //	User FindbyUserName(String user_name);
-	
-
 }
