36451796d9ee338f032875cfeadfaf77390466ba
emball <emball@iastate.edu>
2018-03-01 13:21:43 -0600

    Started threading. Need to implement the messages queue and broadcast

---
+++ ethan/websocketserver/src/main/java/com/bfg/backend/ServerThread.java
@@ -0,0 +1,37 @@
+package com.bfg.backend;
+
+import org.springframework.web.socket.WebSocketSession;
+
+public class ServerThread extends Thread {
+	
+	private Thread t;
+	private WebSocketSession session;
+	
+	ServerThread(WebSocketSession session) {
+		this.session = session;
+	}
+
+	
+	public WebSocketSession getThreadSession() {
+		return session;
+	}
+
+	@Override
+	public void run() {
+		System.out.println("New Thread Started!");
+		
+		for(int i = 0; i < 5; i++) {
+			System.out.println("$$$$$$$$ Thread " + session.getId() + " Loop " + i);
+		}
+	}
+	
+	
+	public void start() {
+		System.out.println("Starting Thread: " + session.getId());
+		if(t == null) {
+			t = new Thread(this, session.getId());
+			t.start();
+		}
+	}
+	
+}
