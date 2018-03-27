ba07904024106049e0f753588473ec3a65943b15
James <jlockard@iastate.edu>
2018-03-27 14:56:59 -0500

    Login is validating and Match screen skeleton is up

--- ethan/websocketserver/src/main/java/com/bfg/backend/repository/BattleStatsRepository.java
+++ ethan/websocketserver/src/main/java/com/bfg/backend/repository/BattleStatsRepository.java
@@ -3,7 +3,7 @@ package com.bfg.backend.repository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import com.bfg.backend.BattleStats;
+import Model.BattleStats;
 
 @Repository
 public interface BattleStatsRepository extends CrudRepository<BattleStats, Long> {
