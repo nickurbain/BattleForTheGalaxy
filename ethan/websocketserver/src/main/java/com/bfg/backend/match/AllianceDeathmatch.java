54a758d42661dc5ddf03b292264733c9a95724b7
emball <emball@iastate.edu>
2018-04-13 14:47:09 -0500

    Working on AllianceDeathmatch, Alliance, & AllianceRepository

--- ethan/websocketserver/src/main/java/com/bfg/backend/match/AllianceDeathmatch.java
+++ ethan/websocketserver/src/main/java/com/bfg/backend/match/AllianceDeathmatch.java
@@ -1,7 +1,39 @@
 package com.bfg.backend.match;
 
-public class AllianceDeathmatch extends AbstractMatch {
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
+import org.springframework.web.socket.WebSocketSession;
+
+public class AllianceDeathmatch extends AbstractMatch {
 	
+	private List<Team> alliances;
+	private Integer killLimit;
+	
+	AllianceDeathmatch() {
+		setMatchType("AllianceDeathmatch");
+		alliances = new CopyOnWriteArrayList<>();
+		killLimit = 10;
+	}
+	
+	
+	@Override
+	public void addPlayer(WebSocketSession player) {
+		super.addPlayerStd(player);
+		Player p = getPlayer(player);
+		
+		/* TODO
+		 * Check which alliance a user is in, use that to add them to a team.
+		 */
+		
+		super.welcomeMessageWithTeam(player, p.getTeam());
+		super.addClientToBC(player);
+	}
+	
+	public void addPlayerToAlliances(Player p) {
+		if(alliances.isEmpty()) {
+			alliances.add(new Team(p.getTeam()));
+		}
+	}
 	
 }
