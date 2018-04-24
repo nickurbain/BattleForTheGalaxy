3c88e0964725498894889bf7d078e436fc40baba
nhurbain <nhurbain@iastate.edu>
2018-04-23 21:16:27 -0500

    Must enter username and password to log in.

--- Workspace/core/src/battle/galaxy/LoginScreen.java
+++ Workspace/core/src/battle/galaxy/LoginScreen.java
@@ -51,7 +51,7 @@ public class LoginScreen extends MasterScreen {
 		title.setFontScale(4f);
 
 		// TextBoxes for this LoginScreen
-		userName = TextBox(skin, "userName", "User Name");
+		userName = TextBox(skin, "userName", "Username");
 		password = TextBox(skin, "password", "Password");
 
 		// Add all elements to this LoginScreen
@@ -97,7 +97,7 @@ public class LoginScreen extends MasterScreen {
 		button.addListener(new ClickListener() {
 			@Override
 			public void clicked(InputEvent event, float x, float y) {
-				if (name.equals("LOGIN") && (userName.getMessageText() != "")) {
+				if (name.equals("LOGIN") && (userName.getText() != "" && userName.getMessageText() != "Username") && password.getMessageText() != "Password") {
 					System.out.println("User Name: " + userName.getText() + ", Password: " + password.getText());
 					UserQueryController.login(userName.getText(), password.getText());
 				} else if (name.equals("REGISTER")) {
