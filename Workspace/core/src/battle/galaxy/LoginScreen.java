061023399c109d31a2d22782db2370f3b75b6ad0
darowski <darowski@iastate.edu>
2018-04-24 10:05:07 -0500

    LoginScreen: added tab traversal through text fields and pressing ENTER can login

--- Workspace/core/src/battle/galaxy/LoginScreen.java
+++ Workspace/core/src/battle/galaxy/LoginScreen.java
@@ -4,6 +4,7 @@ import java.net.UnknownHostException;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
 //import com.badlogic.gdx.scenes.scene2d.ui.Dialog;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
@@ -145,6 +146,22 @@ public class LoginScreen extends MasterScreen {
 					field.setPasswordMode(true);
 					field.setPasswordCharacter('*');
 				}
+			}
+		});
+		field.setFocusTraversal(true);
+		field.addListener(new InputListener() {
+			public boolean keyDown(InputEvent event, int keycode) {
+				if(keycode == Keys.ENTER) {
+					// login normally
+					System.out.println("User Name: " + userName.getText() + ", Password: " + password.getText());
+					if (!userName.getText().contains(" ") && !password.getText().contains(" ")) {
+						UserQueryController.login(userName.getText(), password.getText());
+					}
+					return true;
+				} else {
+					return false;
+				}
+
 			}
 		});
 		return field;
