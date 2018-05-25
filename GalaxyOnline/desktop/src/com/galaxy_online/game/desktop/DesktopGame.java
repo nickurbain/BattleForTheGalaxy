08cdd80f0bcf2411921741f6f14f4072a2b44ae6
nickurbain <nhurbain@iastate.edu>
2018-05-24 21:25:06 -0500

    Cleaned up repository.

--- GalaxyOnline/desktop/src/com/galaxy_online/game/desktop/DesktopGame.java
+++
@@ -1,8 +0,0 @@
-package com.galaxy_online.game.desktop;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
-
-public class DesktopGame {
-	public static void main (String[] args) {
-		new LwjglApplication(new Game(), "Game Menu", 1500, 1500);
-	}
-}
